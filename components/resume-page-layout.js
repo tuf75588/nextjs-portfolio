2a32e40aa0f3621cfa16722855b174558bf4a2de
Andrew Davis <atd285@gmail.com>
2021-03-09 14:31:04 -0500

    begin schema work for resume page

---
+++ components/resume-page-layout.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
+
+import buildSchema from "../lib/buildSchema";
+
+function ResumePageLayout({ meta, children }) {
+  const { description } = meta;
+  const router = useRouter();
+  const metaUrl = `https://andrewdavis.dev/${router.asPath}`;
+  return (
+    <>
+      <Head>
+        {buildSchema({
+          "@context": `http://schema.org`,
+          "@type": `Person`,
+          name: `Andrew Davis`,
+          gender: `male`,
+          email: `develop.it4@gmail.com`,
+        })}
+      </Head>
+      <NextSeo
+        canonical={metaUrl}
+        description={description}
+        openGraph={{
+          title: meta.title,
+          description: meta.description,
+          type: `article`,
+          url: metaUrl,
+        }}
+      />
+    </>
+  );
+}
+
+export default ResumePageLayout;
